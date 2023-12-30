3cecc629f7a09f4ed9ccca5dd3c0138f5f913c5b
KamalSinghKhanna <kamalsinghkhanna2706@gmail.com>
2023-12-30 18:11:07 +0530

    increased the api limit

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ const Home = () => {
   const fetchData = async () => {
     try {
       const response = await axios.get(
-        `https://pokeapi.co/api/v2/pokemon/?limit=100&offset=0`
+        `https://pokeapi.co/api/v2/pokemon/?limit=1000&offset=0`
       );
 
       const detailsPromises = response.data.results.map((item) =>
